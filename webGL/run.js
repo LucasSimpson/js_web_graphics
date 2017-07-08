4e1e4c4b7945021a810b1b2a5721ae2dc03aae18
Lucas Simpson <lucassimpson05@gmail.com>
2017-07-08 10:20:10 -0400

    abstraction success

--- webGL/run.js
+++ webGL/run.js
@@ -1,7 +1,19 @@
 var canvas;
 var gl;
-var squareVerticesBuffer;
-var squareVerticesColorBuffer;
+
+var cubeVerticesBuffer;
+var cubeVerticesColorBuffer;
+var cubeVerticesIndexBuffer;
+var cubeVerticesIndexBuffer;
+var cubeRotation = 0.0;
+var cubeXOffset = 0.0;
+var cubeYOffset = 0.0;
+var cubeZOffset = 0.0;
+var lastCubeUpdateTime = 0;
+var xIncValue = 0.2;
+var yIncValue = -0.4;
+var zIncValue = 0.3;
+
 var mvMatrix;
 var shaderProgram;
 var vertexPositionAttribute;
@@ -25,43 +37,6 @@ function GetWebGLContext(canvas) {
 
   // Note that "webgl" is not available as of Safari version <= 7.0.3
   // So we have to fall back to ambiguous alternatives for it and some other browsers
-}
-
-
-
-//
-// start
-//
-// Called when the canvas is created to get the ball rolling.
-// Figuratively, that is. There's nothing moving in this demo.
-//
-function start() {
-  canvas = document.getElementById("glCanvas");
-
-  initWebGL(canvas);      // Initialize the GL context
-
-  // Only continue if WebGL is available and working
-
-  if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-    // Initialize the shaders; this is where all the lighting for the
-    // vertices and so forth is established.
-
-    initShaders();
-
-    // Here's where we call the routine that builds all the objects
-    // we'll be drawing.
-
-    initBuffers();
-
-    // Set up to draw the scene periodically.
-
-    setInterval(drawScene, 15);
-  }
 }
 
 //
@@ -103,51 +78,87 @@ function initWebGL(canvas) {
   }
 }
 
+
+//
+// start
+//
+// Called when the canvas is created to get the ball rolling.
+//
+function start() {
+  canvas = document.getElementById("glcanvas");
+
+  initWebGL(canvas);      // Initialize the GL context
+
+  // Only continue if WebGL is available and working
+
+  if (gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+    // Initialize the shaders; this is where all the lighting for the
+    // vertices and so forth is established.
+
+    initShaders();
+
+    // Here's where we call the routine that builds all the objects
+    // we'll be drawing.
+
+    initBuffers();
+
+    // Set up to draw the scene periodically.
+
+    setInterval(drawScene, 15);
+  }
+}
+
+
 //
 // initBuffers
 //
 // Initialize the buffers we'll need. For this demo, we just have
-// one object -- a simple two-dimensional square.
+// one object -- a simple two-dimensional cube.
 //
 function initBuffers() {
 
-  // Create a buffer for the square's vertices.
+  var polys = build_polygons();
 
-  squareVerticesBuffer = gl.createBuffer();
+  console.log('polyes:');
+  console.log(polys);
 
-  // Select the squareVerticesBuffer as the one to apply vertex
+  // Create a buffer for the cube's vertices.
+  cubeVerticesBuffer = gl.createBuffer();
+
+  // Select the cubeVerticesBuffer as the one to apply vertex
   // operations to from here out.
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-
-  // Now create an array of vertices for the square. Note that the Z
-  // coordinate is always 0 here.
-
-  var vertices = [
-    1.0,  1.0,  0.0,
-    -1.0, 1.0,  0.0,
-    1.0,  -1.0, 0.0,
-    -1.0, -1.0, 0.0
-  ];
 
   // Now pass the list of vertices into WebGL to build the shape. We
   // do this by creating a Float32Array from the JavaScript array,
   // then use it to fill the current vertex buffer.
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(polys.vertices), gl.STATIC_DRAW);
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  // create buffer for the colors
+  cubeVerticesColorBuffer = gl.createBuffer();
 
-  // Now set up the colors for the vertices
+  // bind buffer to array?
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
 
-  var colors = [
-    1.0,  1.0,  1.0,  1.0,    // white
-    1.0,  0.0,  0.0,  1.0,    // red
-    0.0,  1.0,  0.0,  1.0,    // green
-    0.0,  0.0,  1.0,  1.0     // blue
-  ];
+  // pass the list 
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(polys.colors), gl.STATIC_DRAW);
 
-  squareVerticesColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  // Build the element array buffer; this specifies the indices
+  // into the vertex array for each face's vertices.
+
+  cubeVerticesIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
+
+
+  // Now send the element array to GL
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(polys.indices), gl.STATIC_DRAW);
 }
 
 //
@@ -157,7 +168,6 @@ function initBuffers() {
 //
 function drawScene() {
   // Clear the canvas before we start drawing on it.
-
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // Establish the perspective with which we want to view the
@@ -173,25 +183,56 @@ function drawScene() {
   loadIdentity();
 
   // Now move the drawing position a bit to where we want to start
-  // drawing the square.
+  // drawing the cube.
 
   mvTranslate([-0.0, 0.0, -6.0]);
 
-  // Draw the square by binding the array buffer to the square's vertices
+  // Save the current matrix, then rotate before we draw.
+
+  mvPushMatrix();
+  mvRotate(cubeRotation, [1, 0, 1]);
+  mvTranslate([cubeXOffset, cubeYOffset, cubeZOffset]);
+
+  // Draw the cube by binding the array buffer to the cube's vertices
   // array, setting attributes, and pushing it to GL.
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
   gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
 
   // Set the colors attribute for the vertices.
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
   gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
 
-  // Draw the square.
+  // Draw the cube.
 
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
   setMatrixUniforms();
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+
+  // Restore the original matrix
+
+  mvPopMatrix();
+
+  // Update the rotation for the next draw, if it's time to do so.
+
+  var currentTime = (new Date).getTime();
+  if (lastCubeUpdateTime) {
+    var delta = currentTime - lastCubeUpdateTime;
+
+    cubeRotation += (30 * delta) / 1000.0;
+    cubeXOffset += xIncValue * ((30 * delta) / 1000.0);
+    cubeYOffset += yIncValue * ((30 * delta) / 1000.0);
+    cubeZOffset += zIncValue * ((30 * delta) / 1000.0);
+
+    if (Math.abs(cubeYOffset) > 2.5) {
+      xIncValue = -xIncValue;
+      yIncValue = -yIncValue;
+      zIncValue = -zIncValue;
+    }
+  }
+
+  lastCubeUpdateTime = currentTime;
 }
 
 //
@@ -308,3 +349,152 @@ function setMatrixUniforms() {
   var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
   gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
 }
+
+var mvMatrixStack = [];
+
+function mvPushMatrix(m) {
+  if (m) {
+    mvMatrixStack.push(m.dup());
+    mvMatrix = m.dup();
+  } else {
+    mvMatrixStack.push(mvMatrix.dup());
+  }
+}
+
+function mvPopMatrix() {
+  if (!mvMatrixStack.length) {
+    throw("Can't pop from an empty matrix stack.");
+  }
+
+  mvMatrix = mvMatrixStack.pop();
+  return mvMatrix;
+}
+
+function mvRotate(angle, v) {
+  var inRadians = angle * Math.PI / 180.0;
+
+  var m = Matrix.Rotation(inRadians, $V([v[0], v[1], v[2]])).ensure4x4();
+  multMatrix(m);
+}
+
+
+
+
+
+/**************************************************************************/
+
+
+
+
+/**************************************************************************/
+
+
+function build_cube(x, y, z, sx, sy, sz, color) {
+  var vertices = [
+    // Front face
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+     1.0,  1.0,  1.0,
+     1.0,  1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+
+    // Right face
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0,  1.0,  1.0,
+     1.0, -1.0,  1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0
+  ];
+
+
+  var colors = [
+    [1.0,  1.0,  1.0,  1.0],    // Front face: white
+    [1.0,  0.0,  0.0,  1.0],    // Back face: red
+    [0.0,  1.0,  0.0,  1.0],    // Top face: green
+    [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+    [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+    [1.0,  0.0,  1.0,  1.0]     // Left face: purple
+  ];
+
+  // Convert the array of colors into a table for all the vertices.
+
+  var generatedColors = [];
+
+  for (j=0; j<6; j++) {
+    var c = colors[j];
+
+    // Repeat each color four times for the four vertices of the face
+
+    for (var i=0; i<4; i++) {
+      generatedColors = generatedColors.concat(c);
+    }
+  }
+
+  // link vertices together into triangles. Polygon color
+  // specified by color in similar index
+  var cubeVertexIndices = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23    // left
+  ];
+
+
+  return {
+    vertices: vertices,
+    colors: generatedColors,
+    indices: cubeVertexIndices
+  };
+}
+
+function append(polys, new_polys) {
+  polys.vertices = polys.vertices.concat(new_polys.vertices);
+  polys.colors = polys.colors.concat(new_polys.colors);
+  polys.indices = polys.indices.concat(new_polys.indices);
+
+  return polys;
+}
+
+function build_polygons() {
+  polys = {
+    vertices: [],
+    colors: [],
+    indices: []
+  };
+
+  polys = append(polys, build_cube());
+
+  return polys;
+}
+
+
+
+
+
+
+
+
